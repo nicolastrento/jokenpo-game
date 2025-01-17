dcbc0fef336cc2d61678004a2f74b43358e7a4b3
nicolastrento <nitrento1003@hotmail.com>
2025-01-17 18:22:13 -0300

    subindo projeto: jokenpo-game

---
+++ script.js
@@ -0,0 +1,40 @@
+
+const result = document.querySelector('.result')
+const humanScore = document.querySelector('#human-score')
+const machineScore = document.querySelector('#machine-score')
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+const playHuman = (humanChoice) => {
+
+  playTheGame(humanChoice, playMachine())
+}
+
+const playMachine = () => {
+  const choices = ['pedra', 'papel', 'tesoura']
+  const randomNumber = Math.floor(Math.random() * 3)
+
+  return choices[randomNumber]
+}
+
+const playTheGame = (human, machine) => {
+
+  console.log('Humano: ' + human + 'machine: ' + machine)
+
+  if (human === machine) {
+    result.innerHTML = 'Deu Empate!'
+  } else if (
+    human === 'papel' && machine === 'pedra' || 
+    human === 'pedra' && machine === 'tesoura' ||
+    human === 'tesoura' && machine === 'papel'
+  ) {
+    humanScoreNumber++
+    humanScore.innerHTML = humanScoreNumber
+    result.innerHTML = 'Você Ganhou!'
+  } else {
+    machineScoreNumber++
+    machineScore.innerHTML = machineScoreNumber
+    result.innerHTML = 'Você Perdeu!'
+  }
+}
