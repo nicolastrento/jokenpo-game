6ca7a728236d4606a57c1811ed71e775cb514b24
nicolastrento <nitrento1003@hotmail.com>
2025-01-20 15:49:19 -0300

    atualização: adicionei os enums para deixar o código mais compacto, se houver mais linhas.

--- script.js
+++ script.js
@@ -7,13 +7,19 @@ const machineScore = document.querySelector('#machine-score')
 let humanScoreNumber = 0
 let machineScoreNumber = 0
 
+const GAME_OPTIONS = {
+  PEDRA: 'pedra',
+  PAPEL: 'papel',
+  TESOURA: 'tesoura'
+}
+
 const playHuman = (humanChoice) => {
 
   playTheGame(humanChoice, playMachine())
 }
 
 const playMachine = () => {
-  const choices = ['pedra', 'papel', 'tesoura']
+  const choices = [GAME_OPTIONS.PEDRA, GAME_OPTIONS.PAPEL, GAME_OPTIONS.TESOURA]
   const randomNumber = Math.floor(Math.random() * 3)
 
   return choices[randomNumber]
@@ -28,9 +34,9 @@ const playTheGame = (human, machine) => {
   if (human === machine) {
     result.innerHTML = 'Deu Empate!'
   } else if (
-    human === 'papel' && machine === 'pedra' || 
-    human === 'pedra' && machine === 'tesoura' ||
-    human === 'tesoura' && machine === 'papel'
+    human === GAME_OPTIONS.PAPEL && machine === GAME_OPTIONS.PEDRA || 
+    human === GAME_OPTIONS.PEDRA && machine === GAME_OPTIONS.TESOURA ||
+    human === GAME_OPTIONS.TESOURA && machine === GAME_OPTIONS.PAPEL
   ) {
     humanScoreNumber++
     humanScore.innerHTML = humanScoreNumber
